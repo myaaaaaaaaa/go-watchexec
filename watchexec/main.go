a59fae3e3952d8b2a3b1f1a04bb58d298a2b4720
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-11-15 04:45:59 -0500

    enable watchexec functionality

--- watchexec/main.go
+++ watchexec/main.go
@@ -17,27 +17,32 @@ func isTerminal(f fs.File) bool {
 	return stat.Mode()&fs.ModeCharDevice != 0
 }
 
+func watch(f func(string)) {
+	var w watchexec.Watcher
+	w.FilesPerCycle = 4
+	w.Wait = time.Millisecond * 50
+
+	for {
+		for file := range w.ScanCycles(os.DirFS("."), 100) {
+			if file != "" {
+				f(file)
+			}
+		}
+	}
+}
+
 func main() {
 	args := os.Args[1:]
 
 	if !isTerminal(os.Stdout) {
-		var w watchexec.Watcher
-		w.FilesPerCycle = 4
-		w.Wait = time.Millisecond * 50
-
-		for {
-			for f := range w.ScanCycles(os.DirFS("."), 100) {
-				if f != "" {
-					fmt.Println(f)
-				}
-			}
-		}
+		watch(func(s string) {
+			fmt.Println(s)
+		})
 	}
 	if len(args) > 0 {
-		for {
+		watch(func(_ string) {
 			watchexec.ExecOutput(os.Stderr, args)
-			time.Sleep(time.Second * 2)
-		}
+		})
 	}
 	if isTerminal(os.Stdin) {
 		fmt.Println("stdin is terminal")
