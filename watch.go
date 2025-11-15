297a2328aad9e21b17bc82711f56988ab4a04ded
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-11-14 22:37:09 -0500

    copy walkDirFunc from another project

--- watch.go
+++ watch.go
@@ -1,1 +1,23 @@
 package watchexec
+
+import (
+	"io/fs"
+)
+
+type globber struct {
+	files *[]string
+}
+
+func (g globber) walkDirFunc(p string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return fs.SkipDir
+	}
+	if d.IsDir() && d.Name()[0] == '.' {
+		return fs.SkipDir
+	}
+
+	if !d.IsDir() {
+		*g.files = append(*g.files, p)
+	}
+	return nil
+}
--- watch_test.go
+++ watch_test.go
@@ -1,1 +1,72 @@
 package watchexec
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"strings"
+	"testing"
+	"testing/fstest"
+)
+
+type errorFS struct {
+	fs.FS
+}
+
+func (fsys errorFS) Open(name string) (fs.File, error) {
+	if strings.Contains(name, "error") {
+		return nil, errors.New("error file: " + name)
+	}
+	return fsys.FS.Open(name)
+}
+
+func TestGlobber(t *testing.T) {
+	mapfs := errorFS{fstest.MapFS{
+		"a/f":            &fstest.MapFile{},
+		"b/d/d/d/.f.txt": &fstest.MapFile{},
+		"c/d/.d/d/f.txt": &fstest.MapFile{},
+		".d.txt":         &fstest.MapFile{},
+		".e.txt/d/d/f":   &fstest.MapFile{},
+		".f.txt/d/f.txt": &fstest.MapFile{},
+
+		"y/1/f.txt":       &fstest.MapFile{},
+		"y/2/error/f.txt": &fstest.MapFile{},
+		"y/3/f.txt":       &fstest.MapFile{},
+
+		"z/1/f.txt": &fstest.MapFile{},
+		"z/2/f.txt": &fstest.MapFile{},
+		"z/3/f.txt": &fstest.MapFile{},
+	}}
+
+	assert := func(arg string, want ...string) {
+		t.Helper()
+
+		var got []string
+		err := fs.WalkDir(mapfs, arg, globber{&got}.walkDirFunc)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if fmt.Sprint(got) != fmt.Sprint(want) {
+			t.Error("got", got, "    want", want)
+		}
+	}
+
+	assert("a", "a/f")
+	assert("b", "b/d/d/d/.f.txt")
+	assert("c", "")
+	assert(".d.txt", ".d.txt")
+	assert(".e.txt", "")
+	assert(".f.txt", "")
+
+	assert("y",
+		"y/1/f.txt",
+		"y/3/f.txt",
+	)
+
+	assert("z",
+		"z/1/f.txt",
+		"z/2/f.txt",
+		"z/3/f.txt",
+	)
+}
