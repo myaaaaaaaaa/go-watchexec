7ec8e7ffd91ba0bc4b225113e7a4097624af7f7c
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-11-16 00:58:52 -0500

    TestRepeatChunks()

--- datastructures_test.go
+++ datastructures_test.go
@@ -1,6 +1,8 @@
 package watchexec
 
 import (
+	"bytes"
+	"slices"
 	"testing"
 )
 
@@ -43,4 +45,25 @@ func TestLruPut(t *testing.T) {
 		s = lruPut(s, "b", 1)
 		assertEquals(t, s, "[b]")
 	})
+}
+
+func TestRepeatChunks(t *testing.T) {
+	assert := func(s string, chunkSize, numChunks int, want string) {
+		t.Helper()
+
+		got := bytes.Join(slices.Collect(repeatChunks([]byte(s), chunkSize, numChunks)), []byte(" "))
+		assertEquals(t, string(got), want)
+	}
+
+	assert("hello", 1, 1, "h")
+	assert("hello", 2, 1, "he")
+	assert("hello", 1, 2, "h e")
+	assert("hello", 2, 2, "he ll")
+	assert("hello", 3, 4, "hel lo hel lo")
+
+	assert("", 1, 1, "")
+	assert("", 2, 1, "")
+	assert("", 3, 1, "")
+	assert("", 1, 2, " ")
+	assert("", 1, 3, "  ")
 }
