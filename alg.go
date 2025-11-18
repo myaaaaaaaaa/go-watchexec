290248bfb09ec963b72d236e3d499a2e49001e09
myaaaaaaaaa <103326468+myaaaaaaaaa@users.noreply.github.com>
2025-11-18 14:27:44 -0500

    remove vestigial type

--- alg.go
+++ alg.go
@@ -5,8 +5,6 @@ import (
 	"slices"
 )
 
-type set[T comparable] = map[T]struct{}
-
 func lruPut[T comparable](s []T, value T, cap int) []T {
 	s = slices.DeleteFunc(s, func(e T) bool {
 		return e == value
